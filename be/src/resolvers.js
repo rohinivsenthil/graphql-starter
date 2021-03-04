bad71c7dfb09f02e6146d8e4646b6fd237ccafea
Rohini Senthil <rohinivsenthil@gmail.com>
2021-03-04 09:54:11 +0530

    (feat) add data file write/read

--- be/src/resolvers.js
+++ be/src/resolvers.js
@@ -1,38 +1,32 @@
-const posts = [
-  {
-    id: '1',
-    title: 'title 1',
-    description: 'desc 1',
-    author: 'author 1'
-  },
-  {
-    id: '2',
-    title: 'title 2',
-    description: 'desc 2',
-    author: 'author 2'
-  }
-]
-
+import fs from 'fs';
+import { v4 } from 'uuid';
 
 const resolvers = {
   Query: {
     ping: () => 'Pong',
     getPosts: () => {
-      return posts
+      return JSON.parse(fs.readFileSync('posts.json'));
     },
   },
 
   Mutation: {
     createPost: (_, args) => {
-      console.log(args);
-      return 'Post created successfully'
+      const posts = [...JSON.parse(fs.readFileSync('posts.json')), {...args.data, id: v4()}];
+      fs.writeFileSync('posts.json', JSON.stringify(posts, null, 4))
+      return 'Post created successfully';
     },
     updatePost: (_, args) => {
-      console.log(args);
+      const posts = JSON.parse(fs.readFileSync('posts.json')).map(post =>
+        post.id === args.id ? {...post, ...args.data} : post
+      );
+      fs.writeFileSync('posts.json', JSON.stringify(posts, null, 4))
       return 'Post updated successfully'
     },
     deletePost: (_, args) => {
-      console.log(args);
+      const posts = JSON.parse(fs.readFileSync('posts.json')).filter(post =>
+        post.id !== args.id
+      );
+      fs.writeFileSync('posts.json', JSON.stringify(posts, null, 4))
       return 'Post deleted successfully'
     }
   }
